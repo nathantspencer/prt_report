fd2065596d9103aa095197a67507e2c93e752ed9
nathantspencer <nathantspencer@gmail.com>
2017-08-13 17:44:59 -0400

    added print to report progress, removes irrelevant tweets

--- scrape.py
+++ scrape.py
@@ -1,10 +1,12 @@
 import code
+import datetime
 import json
 import oauth2
 import pandas as pd
 import requests
 
 from collections import namedtuple
+from dateutil import parser
 from keys import MY_KEY, MY_KEY_SECRET, MY_TOKEN, MY_TOKEN_SECRET
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
 
@@ -22,6 +24,10 @@ if __name__ == "__main__":
     # Total number of tweets retrieved will be BATCHES_TO_RETRIEVE * 200
     BATCHES_TO_RETRIEVE = 16
 
+    START_DATE = datetime.date(2016, 1, 9)
+    END_DATE = datetime.date(2016, 5, 5)
+
+    print("Authorizing...")
     consumer = oauth2.Consumer(key=MY_KEY, secret=MY_KEY_SECRET)
     token = oauth2.Token(key=MY_TOKEN, secret=MY_TOKEN_SECRET)
     client = oauth2.Client(consumer, token)
@@ -30,6 +36,7 @@ if __name__ == "__main__":
 
     tweets = []
     for i in range(BATCHES_TO_RETRIEVE):
+        print("Requesting batch " + str(i + 1) + "...")
         content_json = json.loads(content.decode("utf-8"))
         max_id = -1
         for item in content_json:
@@ -44,8 +51,18 @@ if __name__ == "__main__":
         response, content = client.request(request_url, "GET")
 
     data_frame = pd.DataFrame(tweets)
+    print("Removing tweets not reporting PRT up/down status...")
     for i, tweet_text in enumerate(data_frame["text"]):
         if tweet_text[0:15] != "The PRT is down" and tweet_text[0:18] !=  "The PRT is running":
             data_frame = data_frame.drop(i)
+    data_frame = data_frame.reset_index()
+
+    print("Removing tweets outside of start/end date range...")
+    for i, tweet_date in enumerate(data_frame["created_at"]):
+        parsed_datetime = parser.parse(tweet_date)
+        parsed_date = datetime.date(parsed_datetime.year, parsed_datetime.month, parsed_datetime.day)
+        if parsed_date < START_DATE or parsed_date > END_DATE:
+            data_frame = data_frame.drop(i)
+    data_frame = data_frame.reset_index()
 
     print(data_frame)
