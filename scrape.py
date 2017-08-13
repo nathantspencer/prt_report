18381dba9b3540b81d3d81fce99e0a0dbbeb6ee5
nathantspencer <nathantspencer@gmail.com>
2017-08-13 15:52:30 -0400

    grabs batches of tweets, loads into dataframe

--- scrape.py
+++ scrape.py
@@ -1,3 +1,4 @@
+import code
 import json
 import oauth2
 import pandas as pd
@@ -14,19 +15,34 @@ if __name__ == "__main__":
     TOKEN_SECRET = MY_TOKEN_SECRET
 
     BASE_URL = "https://api.twitter.com/1.1/statuses/"
-    TIMELINE_URL = BASE_URL + "user_timeline.json?screen_name=WVUDOT&count=400&trim_user=true"
+    TIMELINE_URL = BASE_URL + "user_timeline.json?screen_name=WVUDOT"
+    COUNT_URL = "&count="
+    MAX_ID_URL = "&max_id="
+
+    # Total number of tweets retrieved will be BATCHES_TO_RETRIEVE * 200
+    BATCHES_TO_RETRIEVE = 16
 
     consumer = oauth2.Consumer(key=MY_KEY, secret=MY_KEY_SECRET)
     token = oauth2.Token(key=MY_TOKEN, secret=MY_TOKEN_SECRET)
     client = oauth2.Client(consumer, token)
-    response, content = client.request(TIMELINE_URL, "GET")
+    request_url = TIMELINE_URL + COUNT_URL + str(200)
+    response, content = client.request(request_url, "GET")
 
     tweets = []
-    json = json.loads(content.decode("utf-8"))
-    for item in json:
-        tweet = {}
-        tweet["id"] = item["id"]
-        tweet["created_at"] = item["created_at"]
-        tweet["text"] = item["text"]
-        tweets.append(tweet)
-    print(tweets)
+    for i in range(BATCHES_TO_RETRIEVE):
+        content_json = json.loads(content.decode("utf-8"))
+        max_id = -1
+        for item in content_json:
+            tweet = {}
+            tweet["id"] = item["id"]
+            tweet["created_at"] = item["created_at"]
+            tweet["text"] = item["text"]
+            if max_id == -1 or tweet["id"] < max_id:
+                max_id = tweet["id"]
+            tweets.append(tweet)
+        request_url = TIMELINE_URL + COUNT_URL + str(200) + MAX_ID_URL + str(max_id - 1)
+        response, content = client.request(request_url, "GET")
+
+    data_frame = pd.DataFrame(tweets)
+
+    # TODO: Cull non-relevant entries by their text content
